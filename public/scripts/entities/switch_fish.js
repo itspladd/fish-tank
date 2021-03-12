86460a04ae6bdabed0cf0fb9fac71fca8f64f317
Paul Ladd <vlendle@gmail.com>
2021-03-12 23:28:37 +0000

    Added unique images for each fish :)

--- public/scripts/entities/switch_fish.js
+++ public/scripts/entities/switch_fish.js
@@ -1,5 +1,9 @@
 class SwitchFish extends Fish {
 
+  constructor(options) {
+    super(options);
+    this.imageUri = '/images/switch-fish.png';
+  }
   onClick(event) {
     this.makeNewVelocity(50);
   }
